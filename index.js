5c77e5510a465aa60ff98ebc960de116746ea51a
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-13 20:52:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   
 }
 
-function destructivelyUpdateObjectsWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object[key] = value;
 }
   
