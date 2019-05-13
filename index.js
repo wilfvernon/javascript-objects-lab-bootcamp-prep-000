e21688487e9558712694217cbf56e1c86f846a0a
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-13 20:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var recipes = {eggs: 2}
 
 function updateObjectWithKeyAndValue(Object, key, value){
-  return Object.assign({}, Object, {key})
+  return Object.assign({}, Object, {[key]: value})
+}
+
   
